710b834ff828f39a7c50524d38607dd12e807ed7
Denny <dennisoneil72@gmail.com>
2024-07-09 03:04:14 -0400

    Create flasktest.py

---
+++ site_flask/flasktest.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect, url_for
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! This is the main page <h1>HELLO</h1>"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run()
