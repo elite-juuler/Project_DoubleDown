763196cffaec60dada5bca8bf90da098f00fe018
Denny <dennisoneil72@gmail.com>
2024-07-12 16:49:44 -0400

    added "about" function

--- site_flask/flasktest.py
+++ site_flask/flasktest.py
@@ -1,20 +1,31 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 import requests
 import json
 
 app = Flask(__name__)
 
+# what is returned when url "/home" is navigated
 @app.route("/")
 def home():
-    return "Hello! This is the main page <h1>HELLO</h1>"
+    return render_template("index.html")
 
+# when url "/home/" is navigated to, redirects user to actual "home" function
+@app.route("/home/")
+def direct_home():
+    return redirect(url_for("home")) # with option to specify argument , param=""
+
+# says hello to whatever url is navigated to
 @app.route("/<name>")
 def user(name):
     return f"Hello {name}!"
 
-@app.route("/admin")
+@app.route("/admin/")
 def admin():
-    return redirect(url_for("home"))
+    return redirect(url_for("user", name="jim bob")) # name here references the value in < > during the @app.route() line
+
+@app.route("/about/")
+def aboutPage():
+    return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
