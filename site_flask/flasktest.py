680c75fe336caa80d7babca9d94d1edb2597e1c2
Denny <dennisoneil72@gmail.com>
2024-07-22 19:24:59 -0400

    7/22 flasktest update

--- site_flask/flasktest.py
+++ site_flask/flasktest.py
@@ -1,16 +1,36 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
-import requests
-import json
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.secret_key = "Mko09ijn"
-app.permanent_session_lifetime = timedelta(days=2)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.permanent_session_lifetime = timedelta(days=2)
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    gender = db.Column(db.String)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
 
 # what is returned when url "/home" is navigated
 @app.route("/")
 def home():
     return render_template("index.html")
+
+# display all users in db
+@app.route("/viewusers")
+def viewusers():
+    return render_template("viewusers.html", values=users.query.all())
 
 # when url "/home/" is navigated to, redirects user to actual "home" function
 @app.route("/home/")
@@ -37,6 +57,15 @@ def login():
     if request.method == "POST":
         varUser = request.form["nm"]
         session["user"] = varUser
+        
+        found_user = users.query.filter_by(name=varUser).first() 
+        if found_user:
+            session["email"] = found_user.email
+        else:
+            usr = users(varUser, "")
+            db.session.add(usr)
+            db.session.commit()
+
         flash(f"Login successful! Welcome, {varUser}!")
         return redirect(url_for("user"))
     else:
@@ -50,11 +79,15 @@ def login():
 def user():
     email = None
     if "user" in session:
-        user = session["user"]
+        varUser = session["user"]
 
         if request.method == "POST":
             email = request.form["email"]
             session["email"] = email
+            found_user = users.query.filter_by(name=varUser).first()
+            found_user.email = email
+            db.session.commit()
+
             flash("Email was saved!")
         else:
             if "email" in session:
@@ -64,6 +97,7 @@ def user():
     else:
         flash("You are not currently logged in.")
         return redirect(url_for("login"))
+
 
 @app.route("/logout")
 def logout():
@@ -76,4 +110,6 @@ def logout():
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
