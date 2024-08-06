d71ccfa38239cab904f0bb6225cd6e704e7415bc
Denny <dennisoneil72@gmail.com>
2024-08-05 20:51:45 -0400

    now handles multiple players
    BEFORE changing play_hand to accept list instead of player

--- main.py
+++ main.py
@@ -51,23 +51,43 @@ def next_card(shoe,currHand,currValues,currHandSum):
     # values back to main program
     return currHand, currHandSum, currValues
 
-def deal(deck): # gives the player then dealer a card 2x
-    PlayerHand = []
-    PlayerValues = []
+def deal(deck,list_playerHands): # gives each player a card, then dealer a card 2x
     DealerHand = []
     DealerValues = []
     shoe = deck
-    PlayerHand, playerHandTotal, PlayerValues = next_card(shoe,PlayerHand,PlayerValues,0)
-    DealerHand, dealerTotal, DealerValues = next_card(shoe,DealerHand,DealerValues,0)
-    PlayerHand, playerHandTotal, PlayerValues = next_card(shoe,PlayerHand,PlayerValues,playerHandTotal)
-    DealerHand, dealerTotal, DealerValues = next_card(shoe,DealerHand,DealerValues,dealerTotal)
-    return DealerHand, dealerTotal, DealerValues, PlayerHand, playerHandTotal, PlayerValues
+    
+    # each player first card
+    for thisHand in list_playerHands:
+        # PlayerHand = []
+        # PlayerValues = []
+        thisHand['cards'], thisHand['handTotal'], thisHand['handValues'] = next_card(shoe,thisHand['cards'], thisHand['handValues'],0)
 
-def check_player_blackjack(playerHandTotal):
-    if card_value(PlayerHand[0]) + card_value(PlayerHand[1]) == 21:
-        return True
+    # dealer 1st card    
+    DealerHand, dealerTotal, DealerValues = next_card(shoe,DealerHand,DealerValues,0)
+        
+    # each player second card
+    for thisHand in list_playerHands:
+        thisHand['cards'], thisHand['handTotal'], thisHand['handValues'] = next_card(shoe,thisHand['cards'], thisHand['handValues'],thisHand['handTotal'])
+    
+    # dealer 2nd card
+    DealerHand, dealerTotal, DealerValues = next_card(shoe,DealerHand,DealerValues,dealerTotal)
+    # print(list_playerHands)
+    # for i in range(handsToDeal):
+    #     list_playerHands = list_playerHands.append[
+    #         {'index': handsToDeal.index(i),'hand': PlayerHand, 'handValues': PlayerValues, 'handTotal': playerHandTotal}
+    #     ]
+    return DealerHand, dealerTotal, DealerValues
+
+def check_player_blackjack(currentPlayer): # checks if player has blackjack and adds to dictionary
+    if card_value(currentPlayer['cards'][0]) + card_value(currentPlayer['cards'][1]) == 21:
+        currentPlayer['hasBJ'] = True
+        # return True
+        return currentPlayer
     else:
-        return False
+        # return False
+        currentPlayer['hasBJ'] = False
+        return currentPlayer
+    
 
 # check_dealer_blackjack - handles dealer showing 10 and dealer showing Ace
 def check_dealer_blackjack(DealerHand,dealerTotal):
@@ -102,28 +122,34 @@ def offer_insurance(): # block that defines actions to take for players taking i
     else:
         return "Y"
 
-def end_of_hand(dealerTotal,playerHandTotal): # block that compares scores and announces result.
+def end_of_hand(dealerTotal,DealerHand,list_playerHands): # block that compares scores and announces result.
     print(">>>Hand is over!")
     time.sleep(1)
     print(f"Dealer: {dealerTotal} | {DealerHand}")
-    print(f"Player: {playerHandTotal} | {PlayerHand}")
-    time.sleep(1)
+    for player in list_playerHands:
+        if dealerTotal > 21 or (player['handTotal'] > dealerTotal and player['handTotal'] <=21):
+            print(f"Player{player['index']+1}: {player['handTotal']} | WIN {player['cards']}")
+        elif dealerTotal == player['handTotal']:
+            print(f"Player{player['index']+1}: {player['handTotal']} | PUSH {player['cards']}")
+        else:
+            print(f"Player{player['index']+1}: {player['handTotal']} | LOSS {player['cards']}")
+        time.sleep(1)
     # if dealer busts or player > dealer
-    if dealerTotal > 21 or (playerHandTotal > dealerTotal and playerHandTotal <= 21):
-        print("You win!")
-    elif dealerTotal == playerHandTotal:
-        print("Push!")
-    else:
-        print("You lose, better luck next time!")
+    # if dealerTotal > 21 or (playerHandTotal > dealerTotal and playerHandTotal <= 21):
+    #     print("You win!")
+    # elif dealerTotal == playerHandTotal:
+    #     print("Push!")
+    # else:
+    #     print("You lose, better luck next time!")
     # reset_hand() # not yet defined
     # playerHandTotal = 0
 
-def get_player_decision(PlayerHand,currValues,playerHandTotal):        
+def get_player_decision(player):        
         # first two cards
-        if len(PlayerHand) == 2:
-            if str(PlayerHand[0])[2:-3] == str(PlayerHand[1])[2:3]:
+        if len(player['cards']) == 2:
+            if str(player['cards'][0])[2:-3] == str(player['cards'][1])[2:3]:
                 # player has pair, can split
-                playerAction = input(f"Hand total: {displayValue(currValues, playerHandTotal)} - Hit/Stand/Double/Split? ").lower()
+                playerAction = input(f"Player{player['index']+1} hand total: {displayValue(player['handValues'], player['handTotal'])} - Hit/Stand/Double/Split? ").lower()
                 if ('h') in playerAction: 
                     decision = "Hit"
                 elif ('st') in playerAction:
@@ -134,7 +160,7 @@ def get_player_decision(PlayerHand,currValues,playerHandTotal):
                     decision = "Split"
                 return decision
             else: # player does not have a pair, cannot split
-                playerAction = input(f"Hand total: {displayValue(currValues, playerHandTotal)} - Hit/Stand/Double? ").lower()
+                playerAction = input(f"Player{player['index']+1} hand total: {displayValue(player['handValues'], player['handTotal'])} - Hit/Stand/Double? ").lower()
                 if ('h') in playerAction: 
                     decision = "Hit"
                 elif ('st') in playerAction:
@@ -144,7 +170,7 @@ def get_player_decision(PlayerHand,currValues,playerHandTotal):
                 return decision
             
         else: # player has more than 2 cards, cannot split or double
-            playerAction = input(f"Hand total: {displayValue(currValues, playerHandTotal)} - Hit/Stand? ").lower()
+            playerAction = input(f"Player{player['index']+1} hand total: {displayValue(player['handValues'], player['handTotal'])} - Hit/Stand? ").lower()
             if ('h') in playerAction: 
                 decision = "Hit"
             elif ('st') in playerAction:
@@ -152,39 +178,41 @@ def get_player_decision(PlayerHand,currValues,playerHandTotal):
             return decision
 
 
-def play_hand(deck,PlayerHand,currValues,playerHandTotal): # block of code that defines the playing of a hand
+def play_hand(deck,player): # block of code that defines the playing of a hand
     
-    while playerHandTotal <= 21:
+    while player['handTotal'] <= 21:
 
-        action = get_player_decision(PlayerHand,currValues,playerHandTotal)
+        action = get_player_decision(player)
         if action == "Hit":
-            deck, PlayerHand, currValues, playerHandTotal = player_hit(deck,PlayerHand,currValues,playerHandTotal)
-            print(f"Player: {PlayerHand} | {displayValue(currValues,playerHandTotal)}")
+            deck, player = player_hit(deck,player)
+            print(f"Player {player['index']+1}: {player['cards']} | {displayValue(player['handValues'],player['handTotal'])}")
             time.sleep(1)
         elif action == "Stand":
-            handAlive = True
-            return deck, PlayerHand, playerHandTotal,handAlive
+            player['handAlive'] = True
+            return deck, player
         elif action == "Double":
             print("Doubley-Do! Good luck...")
             time.sleep(1)
-            deck, PlayerHand, currValues, playerHandTotal = player_hit(deck,PlayerHand,currValues,playerHandTotal)
-            print(f"Player: {PlayerHand} | {displayValue(currValues,playerHandTotal)}")
+            # deck, PlayerHand, currValues, playerHandTotal = player_hit(deck,PlayerHand,currValues,playerHandTotal)
+            deck, player = player_hit(deck,player)
+            print(f"Player {player['index']+1}: {player['cards']} | {displayValue(player['handValues'],player['handTotal'])}")
             time.sleep(1)
-            if playerHandTotal <= 21:
-                handAlive = True
-                return deck, PlayerHand, playerHandTotal,handAlive
+            if player['handTotal'] <= 21:
+                player['handAlive'] = True
+                return deck, player
             else: 
-                handAlive = False
-                return deck, PlayerHand, playerHandTotal,handAlive
+                player['handAlive'] = False
+                return deck, player
         elif action == "Split":
             print("Splitting hands, good luck!")
             time.sleep(1)
-            split_hand(deck,PlayerHand)
+            split_hand(deck,player)
+            break
        
-    if playerHandTotal > 21:
+    if player['handTotal'] > 21:
         print("BUST!")
-        handAlive = False
-        return deck,PlayerHand,playerHandTotal,handAlive
+        player['handAlive'] = False
+        return deck,player
 
 
 # dealer hitting actions for Stand 17 rule variations
@@ -195,27 +223,24 @@ def dealer_action_s17(deck,DealerHand,currValues,dealerTotal):
     while dealerTotal < 17:
         print(f"Dealer has {dealerTotal}, dealer hits...")
         time.sleep(1)
-        deck, DealerHand, currValues, dealerTotal = player_hit(deck,DealerHand,currValues,dealerTotal)
+        DealerHand, dealerTotal, currValues = next_card(deck,DealerHand,currValues,dealerTotal)
         print(f"Dealer hand: {DealerHand} | {displayValue(currValues,dealerTotal)}")
         time.sleep(1)
     if dealerTotal >= 17 and dealerTotal <= 21:
-        # print(deck)
-        # print(DealerHand)
-        # print(dealerTotal)
         return deck, DealerHand, currValues, dealerTotal
     if dealerTotal > 21:
         print("Dealer BUSTS!")
         return deck, DealerHand, currValues, dealerTotal
 
 # pulls card from shoe and appends it to player's hand
-def player_hit(deck,PlayerHand,currValues,playerHandTotal):
+def player_hit(deck,player):
     shoe = deck
-    PlayerHand, playerHandTotal, currValues = next_card(shoe,PlayerHand,currValues,playerHandTotal)
-    return deck, PlayerHand, currValues, playerHandTotal
+    player['cards'], player['handTotal'], player['handValues'] = next_card(shoe,player['cards'],player['handValues'],player['handTotal'])
+    return deck, player
     
 
 # split hands
-def split_hand(deck,playerHand):
+def split_hand(deck,player):
     hand1 = []
     hand1Values = []
     hand1.append(playerHand[0])
@@ -244,6 +269,8 @@ def split_hand(deck,playerHand):
         playerHandTotal = split['handTotal']
         play_hand(shoe,PlayerHand,currValues,playerHandTotal)
         
+
+        # global "playerHandTotal" is not being updated, need way to exit loop of play_hand
         # ADD print functions for (hand1 result: score)
     
     # return activeHands,hand1,hand1Total,hand1Values,hand2,hand2Total,hand2Values
@@ -251,44 +278,81 @@ def split_hand(deck,playerHand):
 
 
 
-
-
 ### Main Program ###
 
-activeHands = 1
-# cardRanks = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'] # true deck
-cardRanks = [9,'6'] # test for Ace logic
+numberOfPlayers = int(input(f"How many players this hand? " ))
+keys = ['index','cards','handValues','handTotal']
+list_playerHands = []
+for i in range(numberOfPlayers):
+    playerHand = {}
+    for key in keys:
+        if key == 'index':
+            playerHand[key] = i
+        elif key == 'cards':
+            playerHand[key] = []
+        elif key == 'handValues':
+            playerHand[key] = []
+        elif key == 'handTotal':
+            playerHand[key] = 0
+    list_playerHands.append(playerHand)
+
+cardRanks = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'] # true deck
+# cardRanks = [9,'6'] # test for Ace logic
 deck = create_deck(cardRanks)
 
 # deal cards to player and dealer
-DealerHand, dealerTotal, DealerValues, PlayerHand, playerHandTotal, PlayerValues = deal(deck)
+DealerHand, dealerTotal, DealerValues = deal(deck,list_playerHands)
 
 print(f"Dealer: {DealerHand[0]} | {card_value(DealerHand[0])}") # dealer has a fair hand
-print(f"Player: {PlayerHand} | {displayValue(PlayerValues,playerHandTotal)}")
+for player in list_playerHands:
+    print(f"Player {player['index']+1}: {player['cards']} | {displayValue(player['handValues'],player['handTotal'])}")
 
-PlayerBJ = check_player_blackjack(playerHandTotal)
+# check for player blackjacks
+for player in list_playerHands:
+    PlayerBJ = check_player_blackjack(player)
+    
+# check for dealer blackjack
 DealerBJ = check_dealer_blackjack(DealerHand,dealerTotal)
-time.sleep(1)
 
 ### Procedures for when someone has Blackjack ###
 
 # player has blackjack, dealer does not
-if PlayerBJ == True and DealerBJ != True:
-    end_of_hand(dealerTotal,playerHandTotal)
+for player in list_playerHands:
+    if player['hasBJ'] == True and DealerBJ != True:
+        # ADD code for the following
+            list_playerHands.remove(player)
+            # add winnings to player
+        # end_of_hand(dealerTotal,playerHandTotal)
 
 # player AND dealer have blackjack
-if PlayerBJ == True and DealerBJ == True:
-    end_of_hand(dealerTotal,playerHandTotal)
+    if player['hasBJ'] == True and DealerBJ == True:
+        continue
+        # ADD code for the following
+            # result is a PUSH
+        # end_of_hand(dealerTotal,playerHandTotal)
 
 # player does NOT have blackjack, but dealer DOES
-if PlayerBJ != True and DealerBJ == True:
-    end_of_hand(dealerTotal,playerHandTotal)
+    if player['hasBJ'] != True and DealerBJ == True:
+    # ADD code for the following
+        # result is a loss for player
+        # subtract wager from player
+        end_of_hand(dealerTotal,player)
+        
 
 # neither player or dealer have blackjack
-if PlayerBJ != True and DealerBJ != True:
-    deck, PlayerHand, playerHandTotal,handAlive = play_hand(deck,PlayerHand,PlayerValues,playerHandTotal)
-    if handAlive == True:
-        deck,DealerHand,DealerValues,dealerTotal = dealer_action_s17(deck,DealerHand,DealerValues,dealerTotal)
-        end_of_hand(dealerTotal,playerHandTotal)
-    else:
-        end_of_hand(dealerTotal,playerHandTotal)
+for player in list_playerHands:
+
+    if player['hasBJ'] != True and DealerBJ != True:
+        deck, player = play_hand(deck,player)
+
+        # if playerhandAlive == True:
+        #     deck,DealerHand,DealerValues,dealerTotal = dealer_action_s17(deck,DealerHand,DealerValues,dealerTotal)
+        #     end_of_hand(dealerTotal,playerHandTotal)
+        # else:
+        #     end_of_hand(dealerTotal,playerHandTotal)
+
+# see if dealer needs to hit (hands alive?)
+if sum(1 for players in list_playerHands if players.get('handAlive') == True) > 0:
+    deck,DealerHand,DealerValues,dealerTotal = dealer_action_s17(deck,DealerHand,DealerValues,dealerTotal)
+
+end_of_hand(dealerTotal,DealerHand,list_playerHands)
